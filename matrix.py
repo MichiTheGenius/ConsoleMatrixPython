83fa186c526835083314b4400fdc07281a2cb55c
MichiTheGenius <litzinger04@gmail.com>
2021-11-06 11:18:12 +0100

    combined the colors from colors.py with matrix.py

--- matrix.py
+++ matrix.py
@@ -1,12 +1,13 @@
 import random
 from time import sleep
+from colors import *
 
 delay = int(input("please enter the delay in ms: ")) / 1000
 number_of_characters = int(input("how many characters do you want: "))
-
+color = RED
 while True:
     for i in range(number_of_characters):
         random_number = random.randint(0,1)
-        print(random_number, end=" ")
+        print(f"{color}{random_number}", end=" ")
     print()
     sleep(delay)
