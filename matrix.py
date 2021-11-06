13d67fe67459c474aac7d34abb702a5e1b1ba2ea
MichiTheGenius <litzinger04@gmail.com>
2021-11-06 11:34:12 +0100

    added user input for choosing colors

--- matrix.py
+++ matrix.py
@@ -4,10 +4,28 @@ from colors import *
 
 delay = int(input("please enter the delay in ms: ")) / 1000
 number_of_characters = int(input("how many characters do you want: "))
-color = RED
+color_input = input("Enter RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN or WHITE for the respective colors: ")
+
+color_output = WHITE
+
+if color_input == "RED":
+    color_output = RED
+elif color_input == "GREEN":
+    color_output = GREEN
+elif color_input == "YELLOW":
+    color_output = YELLOW
+elif color_input == "BLUE":
+    color_output = BLUE
+elif color_input == "MAGENTA":
+    color_output = MAGENTA
+elif color_input == "CYAN":
+    color_output = CYAN
+else:
+    color_output = WHITE
+
 while True:
     for i in range(number_of_characters):
         random_number = random.randint(0,1)
-        print(f"{color}{random_number}", end=" ")
+        print(f"{color_output}{random_number}", end=" ")
     print()
     sleep(delay)
